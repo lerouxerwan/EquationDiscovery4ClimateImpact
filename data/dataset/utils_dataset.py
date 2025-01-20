0b3d89188c2ca8e04f71bf28737745878a405a8f
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2025-01-20 12:34:52 +0100

    feature: start loading X_units and y_unit from a csv

--- data/dataset/utils_dataset.py
+++ data/dataset/utils_dataset.py
@@ -11,6 +11,14 @@ from utils.utils_path import DATASET_CSV_PATH
 def load_dataset(filename_dataset: str) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray,
 Optional[ArrayLike[str]], Optional[ArrayLike[str]], np.ndarray, np.ndarray, list[str], int]:
     df = pd.read_csv(op.join(DATASET_CSV_PATH, filename_dataset), index_col=0)
+    # Extract the row 'UNIT' then remove it from df (if the row exists)
+    if 'UNIT' in df.index:
+        series_units = df.loc['UNIT']
+        X_units = series_units.iloc[1:].to_list()
+        y_units = series_units.iloc[:1].to_list()
+        df = df.iloc[1:, :]
+    else:
+        X_units, y_units = None, None
     y = df.iloc[:, 0].values
     X = df.iloc[:, 1:].values
     ind_test = df.index.str.startswith('RCP45')
@@ -20,13 +28,13 @@ Optional[ArrayLike[str]], Optional[ArrayLike[str]], np.ndarray, np.ndarray, list
     variable_names = [variable_name.replace(' ', '_') for variable_name in variable_names]
     assert df.index.values[0].startswith('HIST')
     index_start_validation = int(df.index.str.startswith('HIST').sum())
-    X_units = None
-    y_units = None
     years = np.array([int(i.split('_')[-1]) for i in df.index.values])
     years_train = years[~ind_test]
     years_test = years[ind_test]
     return X_train, y_train, X_test, y_test, X_units, y_units, years_train, years_test, variable_names, index_start_validation
 
 if __name__ == '__main__':
-    res = load_dataset(r"v4_NPPz_annual_season_GOL4_allDepths_HIST_20_RCP85_94_RCP45_94_all_25_month_season.csv")
+    filename_dataset = r"v4_NPPz_annual_season_GOL4_allDepths_HIST_20_RCP85_94_RCP45_94_all_25_month_season.csv"
+    filename_dataset = r"units_v1.csv"
+    res = load_dataset(filename_dataset)
     print(res[-1])
--- tests/emulator/test_climate_impact_emulator.py
+++ tests/emulator/test_climate_impact_emulator.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pytest
+from sympy import Symbol
 
+from data.dataset.utils_dataset import load_dataset
 from tests.emulator.utils_tests_emulator import load_climate_impact_emulator_for_test, \
     run_three_main_functions, load_X_and_y_for_1D_test
 
@@ -33,3 +35,19 @@ def test_deterministic_and_compute_loss():
     # Assert that the method compute_loss of the emulator work well
     for loss1, loss2 in zip(loss_list, emulator.compute_loss(X, y)):
         np.testing.assert_almost_equal(float(loss1), loss2, decimal=0)
+
+@pytest.mark.xfail
+def test_units_v1():
+    """Test loading of the 7 units from the international system"""
+    emulator = load_climate_impact_emulator_for_test()
+    filename_dataset = 'units_v1.csv'
+    X, y, _, _, X_units, y_units, _, _, variable_names, _ = load_dataset(filename_dataset)
+    assert X_units == ['m', 's', 'mol', 'K', 'A', 'kg', 'cd']
+    assert y_units == ['m']
+    emulator.fit(X, y, variable_names=variable_names, X_units=X_units, y_units=y_units)
+    selected_variable_names = set(emulator.selected_expr.atoms(Symbol))
+    sorted_selected_variable_names = sorted([str(variable_name) for variable_name in set(selected_variable_names)])
+    # The selected expression should only contain the variable 'x1', because it has the same unit as the target
+    assert list(sorted_selected_variable_names) == ['x1']
+
+
