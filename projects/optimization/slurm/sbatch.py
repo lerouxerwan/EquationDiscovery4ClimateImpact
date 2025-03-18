7f2678b092834ceaa8daa23f2dfa8e81cfb23dd0
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2025-03-18 15:45:21 +0100

    refactor: change job name for slurm

--- projects/optimization/slurm/sbatch.py
+++ projects/optimization/slurm/sbatch.py
@@ -35,7 +35,8 @@ class Sbatch(ABC):
 
     @property
     def bash_filepath(self) -> str:
-        return op.join(CURRENT_PATH, f'xp_{self.setting_name}', f'{self.setting_name}.sh')
+        job_name = '_'.join([s[:2] for s in self.setting_name.split('_')])
+        return op.join(CURRENT_PATH, f'xp_{self.setting_name}', f'{job_name}.sh')
 
 
     @property
