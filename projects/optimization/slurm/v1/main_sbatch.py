cc4c403da4ca415d6fa5be39f834bc476b6fa7cd
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2025-03-18 22:56:43 +0100

    refactor: run 1000 niter

--- projects/optimization/slurm/v1/main_sbatch.py
+++ projects/optimization/slurm/v1/main_sbatch.py
@@ -5,7 +5,7 @@ class SBatchWorkflowV1(Sbatch):
 
     @property
     def filepath(self) -> str:
-        return 'projects/optimization/slurm/v1/main_workflow.py'
+        return 'projects/optimization/slurm/v2/main_workflow.py'
 
     @property
     def nb_cores(self) -> int:
@@ -13,10 +13,10 @@ class SBatchWorkflowV1(Sbatch):
 
     @property
     def setting_name(self) -> str:
-        return 'workflow_V1_' + str(self.indices[0])
+        return 'workflow_V2_' + str(self.indices[0])
 
 def main_run_multiple_sbatch():
-    for index in range(4):
+    for index in range(2):
         SBatchWorkflowV1(indices=[index]).run()
 
 
