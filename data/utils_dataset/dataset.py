db00d716a7fcba8f035a24c5ac86676ab95ec1e2
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2025-05-13 12:39:52 +0200

    feature: create feature_dataset

--- data/utils_dataset/dataset.py
+++ data/utils_dataset/dataset.py
@@ -8,6 +8,7 @@ from data.utils_dataset.utils_dataset_values import load_dataset_values
 from data.utils_dataset.validation_split import ValidationSplit
 from emulator.utils_plots.plot_by_rcp.plot_time_series_climato import _plot_climatological_time_series
 from emulator.utils_plots.plot_by_rcp.utils_plot_by_rcp import load_rcp_name_to_list_of_years_and_y_and_color_and_label
+from utils.utils_log import log_info
 
 
 @dataclass
@@ -23,9 +24,24 @@ class Dataset(object):
         (self.X_train, self.y_train, self.X_test, self.y_test, self.years_train, self.years_test,
          self.X_units, self.y_units, self.X_labels, self.y_labels, self.X_variables_names, self.y_variable_names,
          self.validation_mask) = self.values
+        # Check and display
+        self.check()
+        log_info(str(self))
+
+    def check(self):
+        """Check values are consistent between themselves"""
+        assert len(self.X_units) == self.X_train.shape[1] == self.X_test.shape[1]
+        assert len(self.X_units) == len(self.X_labels) == len(self.X_variables_names)
+
+    def __str__(self):
+        return (f"Dataset with {self.X_train.shape[1]} features, "
+                f"{self.X_train.shape[0]} train datapoints, {self.X_test.shape[0]} test datapoints")
 
     def plot_values_feature(self, ax: Axes, feature_index: int):
         self.plot_values(ax, self.X_train[:, feature_index], self.X_test[:, feature_index], self.X_labels[feature_index])
+
+    def plot_values_target(self, ax: Axes):
+        self.plot_values(ax, self.y_train, self.y_test, self.y_labels[0])
 
     def plot_values(self, ax: Axes, values_train: np.ndarray, values_test: np.ndarray, label: str):
         rcp_name_to_list_of_years_and_y_and_color_and_label = load_rcp_name_to_list_of_years_and_y_and_color_and_label(
--- projects/paper/section_data/main_plot_data.py
+++ projects/paper/section_data/main_plot_data.py
@@ -8,17 +8,14 @@ def main_plot_data(show=False):
     # Load axis and dataset
     fig, (ax1, ax2) = subplots_custom(1, 2, wspace=0.15)
     dataset =  dataset_npp_season_v1
-    (X_train, y_train, X_test, y_test, years_train, years_test,
-        X_units, y_units, X_labels, y_labels, X_variables_names, y_variable_names,
-        validation_mask) = dataset.values
 
     # Add two plots
-    column_index = X_variables_names.index('SST_JJA')
-    sst_train = X_train[:, column_index] - 273.15
-    sst_test = X_test[:, column_index] - 273.15
-    label_sst = X_labels[column_index].replace('(K)', '($^o$C)')
+    column_index = dataset.X_variables_names.index('SST_JJA')
+    sst_train = dataset.X_train[:, column_index] - 273.15
+    sst_test = dataset.X_test[:, column_index] - 273.15
+    label_sst = dataset.X_labels[column_index].replace('(K)', '($^o$C)')
     dataset.plot_values(ax1, sst_train, sst_test, label_sst)
-    dataset.plot_values(ax2, y_train, y_test, y_labels[0])
+    dataset.plot_values_target(ax2)
 
     # Some additional things for Slides
     # for ax in [ax1, ax2]:
@@ -32,15 +29,10 @@ def main_plot_data(show=False):
 def main_plot_all_features(show=False):
     # Load axis and dataset
     dataset =  dataset_npp_season_v1
-    (X_train, y_train, X_test, y_test, years_train, years_test,
-        X_units, y_units, X_labels, y_labels, X_variables_names, y_variable_names,
-        validation_mask) = dataset.values
     variable_names = ['SSH_DJF', 'SSS_MAM', 'Shortwave_DJF', 'MerWindStr_MAM']
     for variable_name in variable_names:
         ax = plt.gca()
-        column_index = X_variables_names.index(variable_name)
-        label_name = X_labels[column_index]
-        dataset.plot_values(ax, X_train[:, column_index], X_test[:, column_index], label_name)
+        dataset.plot_values_feature(ax, dataset.X_variables_names.index(variable_name))
         show_or_save_plot(f'data_{variable_name}', show)
 
 
