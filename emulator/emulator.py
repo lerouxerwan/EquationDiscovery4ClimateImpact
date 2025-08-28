fb43383e03959e8955afd4041ce8962ada74b67a
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2025-08-28 12:27:10 +0200

    refactor: add main_example1.py

--- emulator/emulator.py
+++ emulator/emulator.py
@@ -240,6 +240,7 @@ class Emulator(PySRRegressor):
             X_validation, y_validation = get_X_and_y(X, y, validation_mask, validation_set=True)
             self.equations_['validation_loss'] = self.compute_loss_list(X_validation, y_validation)
             #  Set the index for the 'validated' model selection using the validation set
+            log_info(f'validation loss list= {self.validation_loss_list}')
             self.index_for_validated_model_selection_ = np.nanargmin(self.validation_loss_list)
 
         return self
