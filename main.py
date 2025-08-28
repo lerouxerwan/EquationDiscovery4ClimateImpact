1f53fb2da3e08d1468d73968b3a53ab66efc5393
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2025-08-28 21:25:52 +0200

    feature: final version of the code before posting

--- main.py
+++ main.py
@@ -3,16 +3,7 @@ from pysr import PySRRegressor
 from utils_dataset import variable_names, X_units, y_units, X, y
 from utils_model import params
 
+model = PySRRegressor(**params)
+model.fit(X, y, variable_names=variable_names, X_units=X_units, y_units=y_units)
+print(f'{len(model.equations_)} equations with complexity: {model.equations_['complexity'].to_list()}')
 
-def main():
-    model = PySRRegressor(**params)
-    model.fit(X, y, variable_names=variable_names, X_units=X_units, y_units=y_units)
-
-    print(X.sum())
-    print(y.sum())
-    print(len(model.equations_))
-    print(model.equations_['complexity'].to_list())
-
-
-if __name__ == '__main__':
-    main()
