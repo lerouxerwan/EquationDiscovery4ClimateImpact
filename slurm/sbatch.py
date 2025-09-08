b43080693284e1a10d5e1a94714a13ea500d76b2
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2025-09-08 13:03:26 +0200

    refactor: update sbatch to activate mamba venv again

--- slurm/sbatch.py
+++ slurm/sbatch.py
@@ -80,8 +80,7 @@ class Sbatch(ABC):
             # Lines of the bash file
             lines = [
                 '#!/bin/bash',
-                # 'source /homes/e23lerou/miniforge3/bin/activate',
-                'eval "$(/homes/e23lerou/miniforge3/bin/mamba shell hook)"',
+                'eval "$(/homes/e23lerou/miniforge3/condabin/mamba shell hook --shell bash)"',
                 'mamba activate venv',
                 self.python_exec,
                 f'rm {self.bash_filepath}'
