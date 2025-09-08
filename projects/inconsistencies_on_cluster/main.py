a3c885a71ad1c99f493dac06d9bc93c389751b57
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2025-09-08 09:45:00 +0200

    refactor: remove main files to compare the code between the local machine and the remote cluster

--- projects/inconsistencies_on_cluster/main.py
+++
@@ -1,9 +0,0 @@
-from pysr import PySRRegressor
-
-from utils_dataset import variable_names, X_units, y_units, X, y
-from utils_model import params
-
-model = PySRRegressor(**params)
-model.fit(X, y, variable_names=variable_names, X_units=X_units, y_units=y_units)
-print(f'{len(model.equations_)} equations with complexity: {model.equations_['complexity'].to_list()}')
-
