0d7fba3a75bbce3ba8fb7c60b3fb8832fd1f3901
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2025-09-08 12:16:58 +0200

    refactor: update main.py to run sensitivity analysis

--- slurm/validation_workflow/main.py
+++ slurm/validation_workflow/main.py
@@ -17,9 +17,9 @@ class SbatchValidationWorkflow(Sbatch):
 
 
 def main():
-    for n_iter in [10]:
+    for n_iter in [100]:
         # for nb_top_hyperparameters in range(1, 11):
-        for nb_top_hyperparameters in [1, 2, 3, 4, 5]:
+        for nb_top_hyperparameters in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
             indices = [0, 0, n_iter, nb_top_hyperparameters, 0]
             sbatch = SbatchValidationWorkflow(indices)
             sbatch.run()
