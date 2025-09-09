99508cd24c86f4b63c9733af5916d858d1404dfc
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2025-09-09 07:53:29 +0200

    feature: add main_sensitivity_nb_top_hyperparameters.py; add a mechanism to load the marginal search only once

---
+++ projects/experiments/nb_top_hyperparameters/main_sensitivity_nb_top_hyperparameters.py
@@ -0,0 +1,29 @@
+from matplotlib import pyplot as plt
+
+from data.utils_dataset.validation_split import ValidationSplit
+from projects.paper.section_results.validation_workflow import ValidationWorkflow
+
+
+def main_sensitivity_nb_top_hyperparameters():
+    model_selection = ['best', 'validated'][0]
+    validation_split = [ValidationSplit.START, ValidationSplit.SYMMETRICAL, ValidationSplit.END,
+                         ValidationSplit.RCP_START, ValidationSplit.EXTREME][0]
+    n_iter = 10
+    nb_top_hyperparameters_list = [1, 2, 3]
+    # Load a single marginal search, with the largest nb_top_hyperparameters
+    validation_workflow = ValidationWorkflow(validation_split, n_iter, max(nb_top_hyperparameters_list), model_selection)
+    sorted_param_names = validation_workflow.sorted_param_names
+    top_emulator_with_search_marginal = validation_workflow.top_emulator_with_search_marginal
+    # Create the plot
+    rmse_test_list = []
+    for nb_top_hyperparameters in nb_top_hyperparameters_list:
+        validation_workflow = ValidationWorkflow(validation_split, n_iter, nb_top_hyperparameters, model_selection,
+                                                 sorted_param_names=sorted_param_names,
+                                                 top_emulator_with_search_marginal=top_emulator_with_search_marginal)
+        rmse_test_list.append(validation_workflow.rmse_test)
+    ax = plt.gca()
+    ax.plot(nb_top_hyperparameters_list, rmse_test_list)
+    plt.show()
+
+if __name__ == '__main__':
+    main_sensitivity_nb_top_hyperparameters()
--- projects/paper/section_results/validation_workflow.py
+++ projects/paper/section_results/validation_workflow.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
 from functools import cached_property
-from typing import Any, OrderedDict
+from typing import Any, OrderedDict, Optional
 
 import pandas as pd
 
 from data.utils_dataset.dataset import Dataset
 from data.utils_dataset.validation_split import ValidationSplit
+from emulator.emulator import Emulator
 from emulator.emulator_with_search import EmulatorWithSearch
 from plot.utils_metric.metric import Metric
 from plot.workflow import fit, compute_loss_test
@@ -21,10 +22,16 @@ class ValidationWorkflow(object):
     model_selection: str
     validation_size: float = 0.3
     fast: bool = False
+    sorted_param_names: Optional[list[str]] = None
+    top_emulator_with_search_marginal: Optional[EmulatorWithSearch] = None
+
 
     def __post_init__(self):
         # Load dataset
         self.dataset = Dataset("NPP_season.csv", "RCP85", "RCP45", self.validation_size, self.validation_split)
+        # Run/Load marginal search if needed
+        if self.sorted_param_names is None:
+            self.sorted_param_names, self.top_emulator_with_search_marginal = self.run_marginal_search()
         # Load top emulator
         self.emulator = self.search_for_top_emulator()
         log_info('Fit top emulator')
@@ -33,8 +40,29 @@ class ValidationWorkflow(object):
         self.rmse_test = compute_loss_test(self.emulator, self.dataset, Metric.RMSE)
 
     def search_for_top_emulator(self) -> EmulatorWithSearch:
+        # Run a random search with respect to these top hyperparameters
+        top_param_names = self.sorted_param_names[:self.nb_top_hyperparameters]
+        log_info(f'Start random search with: {top_param_names}')
+        param_grid = {param_name: param_value for param_name, param_value in self.get_param_name_to_values().items()
+                      if param_name in top_param_names}
+        params_search = {'param_grid': param_grid, 'search_style': 'random', 'n_iter': self.n_iter, 'n_jobs': -1}
+        emulator_with_search_random = EmulatorWithSearch(**self.params_emulator, **params_search)
+        fit(emulator_with_search_random, self.dataset)
+        log_info(f'Run directory: {emulator_with_search_random.run_.run_directory}')
+        rmse_validation_from_random_search = emulator_with_search_random.selected_validation_rmse
+        rmse_validation_from_marginal_search = self.top_emulator_with_search_marginal.selected_validation_rmse
+        log_info(f'Top RMSE validation from random search={rmse_validation_from_random_search}')
+        # Return the emulator that performs best on the validation set
+        if  rmse_validation_from_random_search < rmse_validation_from_marginal_search:
+            log_info(f'Emulator from random search performs best')
+            return emulator_with_search_random
+        else:
+            log_info(f'Emulator from marginal search performs best')
+            return self.top_emulator_with_search_marginal
+
+    def run_marginal_search(self) -> tuple[list[str], EmulatorWithSearch]:
         param_name_to_values = self.get_param_name_to_values()
-        # Run marginal search
+        #  Run marginal search
         log_info(f'Start marginal search with: {list(param_name_to_values.keys())}')
         param_name_to_emulator_with_search: dict[str, EmulatorWithSearch] = {}
         for param_name, param_values in param_name_to_values.items():
@@ -43,30 +71,16 @@ class ValidationWorkflow(object):
             emulator_with_search_marginal = EmulatorWithSearch(**self.params_emulator, **param_search)
             fit(emulator_with_search_marginal, self.dataset)
             param_name_to_emulator_with_search[param_name] = emulator_with_search_marginal
-        # Select the top hyperparameters with respect to their validation loss
+        #  Select the top hyperparameters with respect to their validation loss
         key = lambda n: param_name_to_emulator_with_search[n].selected_validation_rmse
-        top_param_names = list(sorted(list(param_name_to_values.keys()), key=key))[:self.nb_top_hyperparameters]
-        top_emulator_with_search_marginal: EmulatorWithSearch = param_name_to_emulator_with_search[top_param_names[0]]
-        rmse_validation_from_marginal_search = top_emulator_with_search_marginal.selected_validation_rmse
-        log_info(f'Top Params: {top_param_names}')
-        log_info(f'Top RMSE validation: {[key(param_name) for param_name in top_param_names]}')
-        # Run a random search with respect to these top hyperparameters
-        log_info(f'Start random search with: {top_param_names}')
-        param_grid = {param_name: param_value for param_name, param_value in param_name_to_values.items()
-                      if param_name in top_param_names}
-        params_search = {'param_grid': param_grid, 'search_style': 'random', 'n_iter': self.n_iter, 'n_jobs': -1}
-        emulator_with_search_random = EmulatorWithSearch(**self.params_emulator, **params_search)
-        fit(emulator_with_search_random, self.dataset)
-        log_info(f'Run directory: {emulator_with_search_random.run_.run_directory}')
-        rmse_validation_from_random_search = emulator_with_search_random.selected_validation_rmse
-        log_info(f'Top RMSE validation from random search={rmse_validation_from_random_search}')
-        # Return the emulator that performs best on the validation set
-        if  rmse_validation_from_random_search < rmse_validation_from_marginal_search:
-            log_info(f'Emulator from random search performs best')
-            return emulator_with_search_random
-        else:
-            log_info(f'Emulator from marginal search performs best')
-            return top_emulator_with_search_marginal
+        sorted_param_names = list(sorted(list(param_name_to_values.keys()), key=key))
+        top_emulator_with_search_marginal: EmulatorWithSearch = param_name_to_emulator_with_search[
+            sorted_param_names[0]]
+        log_info('Ranking from marginal search:')
+        for rank, param_name in enumerate(sorted_param_names, 1):
+            rmse_validation = round(param_name_to_emulator_with_search[param_name].selected_validation_rmse, 2)
+            log_info(f'#{rank}: {param_name} with RMSE validation = {rmse_validation}')
+        return sorted_param_names, top_emulator_with_search_marginal
 
     @cached_property
     def params_emulator(self) -> dict[str, Any]:
