a41ddc7235ee6aea1c5b0f88a32b2f95afa2b170
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2025-09-10 20:06:04 +0200

    refactor: fix bash file to test compatibility

--- projects/mre_for_cluster/run_main_with_venv.sh
+++ projects/mre_for_cluster/run_main_with_venv.sh
@@ -5,4 +5,4 @@ export PATH="/Odyssey/private/e23lerou/shared_install/julia-1.11.6/bin:$PATH"
 export JULIA_DEPOT_PATH="/Odyssey/private/e23lerou/shared_install/.julia"
 export PYTHONPATH="${PYTHONPATH}:/Odyssey/private/e23lerou/test_server/EquationDiscovery4ClimateImpact"
 mamba activate /Odyssey/private/e23lerou/shared_install/venv
-python projects/mre_cluster/main.py
+python projects/mre_for_cluster/main.py
