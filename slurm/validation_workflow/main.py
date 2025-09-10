2a420dfeb14fc726918922cb6af7408e138b2925
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2025-09-10 21:58:44 +0200

    feature: prepare script for 112

--- slurm/validation_workflow/main.py
+++ slurm/validation_workflow/main.py
@@ -26,9 +26,9 @@ def main_fast():
 
 
 def main():
-    for n_iter in [1000]:
+    for n_iter in [100]:
         # for nb_top_hyperparameters in range(1, 11):
-        for nb_top_hyperparameters in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10][:]:
+        for nb_top_hyperparameters in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10][-3:]:
             indices = [0, 0, n_iter, nb_top_hyperparameters, 0]
             sbatch = SbatchValidationWorkflow(indices)
             sbatch.run()
