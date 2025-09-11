4ecad459beb066fa4c0e8d9d3540a9a19e589305
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2025-09-11 11:21:28 +0200

    refactor: modify sbatch

--- slurm/sbatch.py
+++ slurm/sbatch.py
@@ -82,10 +82,15 @@ class Sbatch(ABC):
             lines = [
                 '#!/bin/bash',
                 # path to avoid mamba trying to load things from the HomeDir
-                'export CONDA_ENVS_DIRS="/homes/e23lerou/shared_space/shared_install"',
-                'export CONDA_RC_PATH="/dev/null"',
-                'export CONDA_PKGS_DIRS="/homes/e23lerou/shared_space/shared_install/miniforge3/pkgs"',
-                'export CONDA_NO_PLUGINS=true',
+                # 'export CONDA_ENVS_DIRS="/homes/e23lerou/shared_space/shared_install"',
+                # 'export CONDA_RC_PATH="/dev/null"',
+                # 'export CONDA_PKGS_DIRS="/homes/e23lerou/shared_space/shared_install/miniforge3/pkgs"',
+                # 'export CONDA_NO_PLUGINS=true',
+                'source /homes/e23lerou/shared_space/shared_install/miniforge3/etc/profile.d/conda.sh"',
+                'source /homes/e23lerou/shared_space/shared_install/miniforge3/etc/profile.d/mamba.sh"',
+                'mamba activate',
+                'conda config --set envs_dirs /homes/e23lerou/shared_space/shared_install'
+                'conda config --set pkgs_dirs /homes/e23lerou/shared_space/shared_install/miniforge3/pkgs'
                 # mamba (for the python and julia environment)
                 f'echo "here"',
                 f'source /Odyssey/private/e23lerou/shared_install/miniforge3/bin/activate',
