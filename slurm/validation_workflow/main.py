330df348ae64a97bc17c0b4b1cc8299d2c8bebcc
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2025-09-25 16:59:32 +0200

    refactor: run with index equal to 2

--- slurm/validation_workflow/main.py
+++ slurm/validation_workflow/main.py
@@ -13,13 +13,13 @@ class SbatchValidationWorkflow(Sbatch):
 
     @property
     def nb_cores(self) -> int:
-        return 1
+        return 40
 
 
 def main_validation_size():
-    for n_iter in [1]:
+    for n_iter in [1000]:
         for nb_top_hyperparameters in [5]:
-            for index in [2, 3, 4, 5, 6, 7, 8]:
+            for index in [2, 3, 4, 5, 6, 7, 8][:1]:
                 indices = [0, 0, n_iter, nb_top_hyperparameters, index]
                 sbatch = SbatchValidationWorkflow(indices)
                 sbatch.run()
