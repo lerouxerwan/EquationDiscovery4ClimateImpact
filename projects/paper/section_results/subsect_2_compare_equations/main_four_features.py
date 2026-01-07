2e395e846b37e7d2f625a4f5e9507eb6beb3eda1
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2026-01-07 16:48:21 +0100

    refactor: improve four features plot

--- projects/paper/section_results/subsect_2_compare_equations/main_four_features.py
+++ projects/paper/section_results/subsect_2_compare_equations/main_four_features.py
@@ -9,10 +9,12 @@ from utils.utils_plot import get_subplots, show_or_save_plot
 def main_four_features(show: Optional[bool] = False) -> None:
     """Plot the features in the selected equation"""
     dataset =  get_dataset()
-    selected_names = ['SST_DJF', 'Shortwave_DJF', 'SST_MAM', 'SSS_MAM']
+    selected_names = ['SSS_MAM', 'SST_MAM', 'SST_DJF', 'Shortwave_DJF']
     selected_feature_indexes = [dataset.X_variable_names.index(selected_name) for selected_name in selected_names]
+    name_to_index = dict(zip(selected_names, selected_feature_indexes))
 
-    for index in selected_feature_indexes[::2]:
+    for name in ['SST_MAM', 'SST_DJF']:
+        index = name_to_index[name]
         dataset.X_test[:, index] -= 273.15
         dataset.X_train[:, index] -= 273.15
         dataset.X_labels[index] = dataset.X_labels[index].replace('(K)', '($^o$C)')
@@ -28,6 +30,8 @@ def main_four_features(show: Optional[bool] = False) -> None:
             for j, ax in enumerate(ax_row):
                 selected_feature_index = selected_feature_indexes[i * ncols + j]
                 plot_values_feature(ax, dataset, selected_feature_index)
+                letter = 'abcd'[i * 2 + j]
+                ax.text(0.5, 0.96, f'({letter})', weight="bold", fontsize=10, transform=ax.transAxes)
     show_or_save_plot(f'four_features', show)
 
 
