8d2e620d89811ae4dad298a2f258ac1cf41b4995
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2026-03-10 15:50:00 +0100

    feature: upddate list of plots in main_paper.py

--- projects/paper/main_paper.py
+++ projects/paper/main_paper.py
@@ -1,3 +1,4 @@
+from projects.paper.section_appendix.main_seasonal_repartition import main_plot_seasonal_repartition_three_pies
 from projects.paper.section_data.main_plot_data import main_plot_data
 from projects.paper.section_appendix.main_pareto_front_example import main_example_1d
 from projects.paper.section_results.subsect_1_compare_optimization.main_compare_optimization_methods import \
@@ -8,6 +9,8 @@ from projects.paper.section_results.subsect_1_compare_optimization.main_equation
 from projects.paper.section_results.subsect_2_compare_equations.main_four_features import main_four_features
 from projects.paper.section_results.subsect_2_compare_equations.main_frequency_variable_name import \
     main_plot_frequency_variable
+from projects.paper.section_results.subsect_2_compare_equations.main_plot_signed_frequency import \
+    main_plot_signed_frequency
 from projects.paper.section_results.subsect_3_analyze_best_equation.main_diagnosis_baseline_equation_with_four_features import \
     main_diagnosis_baseline_equation_with_four_selection_features
 from projects.paper.section_results.subsect_3_analyze_best_equation.main_diagnosis_best_equation import \
@@ -21,14 +24,16 @@ def main_paper(show: bool):
         # Appendix
         main_example_1d,
         # Results
-        main_compare_optimization_methods_v2,
-        main_plot_frequency_variable,
+        # main_compare_optimization_methods_v2,
+        # main_plot_frequency_variable,
+        main_plot_signed_frequency,
         main_four_features,
+        main_plot_seasonal_repartition_three_pies,
         # Diagnosis of equation for the last section
         # main_diagnosis_best_equation,
         main_diagnosis_baseline_equation_with_four_selection_features,
         # Last plot of results must be 'main_equation_table' to retrieve the output in the stdout/terminal
-        main_equation_table
+        main_equation_table,
     ]
     for plot_function in plot_functions:
         plot_function(show)
