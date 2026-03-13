7e043d82627ee597d41fe2212ded3d75fdfeb598
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2026-03-13 12:38:49 +0100

    feature: create first version of goy dataset csv

--- data/create_goy_dataset_csv.py
+++ data/create_goy_dataset_csv.py
@@ -8,14 +8,20 @@ from utils.utils_path import DATA_PATH
 
 VERSION_GOY_DATASET = 1
 
-def get_filepath_dataset_csv(nb_variables: int):
-    return Path(DATA_PATH) / 'dataset' / f'GOY_N{nb_variables}_v{VERSION_GOY_DATASET}.csv'
+def get_filepath_dataset_csv(nb_variables: int, with_validation: bool):
+    filepath = Path(DATA_PATH) / 'dataset' / f'GOY_N{nb_variables}_v{VERSION_GOY_DATASET}'
+    filepath = str(filepath)
+    if with_validation:
+        filepath += '_withVal'
+    filepath += '.csv'
+    return filepath
 
 
-def main_create_goy_dataset(nb_variables: int):
-    filepath_dataset_csv = get_filepath_dataset_csv(nb_variables)
+def main_create_goy_dataset(nb_variables: int, with_validation: bool = False):
+    filepath_dataset_csv = get_filepath_dataset_csv(nb_variables, with_validation)
     if not op.exists(filepath_dataset_csv):
-        df_list = [load_goy_simu_dat(nb_variables, simu_id) for simu_id in [1, 2, 3][:1]]
+        simu_ids = [1, 2, 3] if with_validation else [1, 3]
+        df_list = [load_goy_simu_dat(nb_variables, simu_id) for simu_id in simu_ids]
         df = pd.concat(df_list, axis=0)
         print(df.shape)
         print(df.head())
@@ -23,5 +29,6 @@ def main_create_goy_dataset(nb_variables: int):
 
 
 if __name__ == '__main__':
-    for nb_variables in [10, 22][:1]:
-        main_create_goy_dataset(nb_variables)
+    for with_validation in [False]:
+            for nb_variables in [10, 22][:]:
+                main_create_goy_dataset(nb_variables, with_validation)
--- data/load_goy_simu_dat.py
+++ data/load_goy_simu_dat.py
@@ -1,9 +1,15 @@
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 from utils.utils_path import DATA_PATH
 
+simu_id_to_index_name = {
+    1: 'Train',
+    2: 'Val',
+    3: 'Test',
+}
 
 def load_goy_simu_dat(nb_variables: int, simu_id: int) -> pd.DataFrame:
     """Returns a Dataframe with X rows containing nb_variables values
@@ -13,15 +19,51 @@ def load_goy_simu_dat(nb_variables: int, simu_id: int) -> pd.DataFrame:
     filepath_simu_dat = Path(DATA_PATH) / f'GOY/Simu_N{nb_variables}_{simu_id}/data.dat'
 
     data = []
+    # # Remove the first 100_000 lines, because the code does not have the time to stabilize
+    # start_line_id = 100_000
+    # end_line_id = None
+    # with open(filepath_simu_dat, 'r') as file:
+    #     for line_id, line in enumerate(file):
+    #         if line_id >= start_line_id:
+    #             s = line.strip()
+    #             #  Keep only even columns that correspond to the real part (the odd columns are the imaginary part)
+    #             row = [float(v) for v in s.split()[::2]]
+    #             data.append(row)
+    #             if end_line_id is not None:
+    #                 assert isinstance(end_line_id, int)
+    #                 if line_id > end_line_id:
+    #                     break
+
+    # Extract all data
     with open(filepath_simu_dat, 'r') as file:
-        for line_id, line in enumerate(file):
+        for line in file:
             s = line.strip()
+            #  Keep only even columns that correspond to the real part (the odd columns are the imaginary part)
             row = [float(v) for v in s.split()[::2]]
             data.append(row)
-            if line_id > 9:
-                break
-    df = pd.DataFrame(data, columns=[f'x{i}' for i in range(len(row))])
-    # assert df.shape == (1_000_000, nb_variables)
+    index_name = simu_id_to_index_name[simu_id].upper()
+    index = [f'{index_name}_{i}' for i in range(len(data))]
+    df = pd.DataFrame(data, index=index, columns=[f'x{i}' for i in range(len(row))])
+    assert df.shape == (1_000_000, nb_variables)
+
+
+    #  Compute the temporal difference di (and put it as the first column, and remove the column i)
+    # D'après carlos il faut predire la difference temporelle di a partir de x0,x1,x2 ...,xi-1,xi+1,...,xn
+    # et je peux prendre i=7 ou 8 quand N=22 et i=4 ou 5 quand N=10"""
+    i  = 4 if nb_variables == 10 else 7
+    temporal_difference_values = df.iloc[1:, i].values - df.iloc[:-1, i].values
+    temporal_difference_values = np.append(temporal_difference_values, [np.nan])
+    # print(len(temporal_difference_values))
+    temporal_difference_i = pd.Series(index=df.index, data=temporal_difference_values)
+    df.insert(0, f'd{i}', temporal_difference_i)
+    assert df.shape == (1_000_000, nb_variables + 1)
+
+    # Remove the column xi  the first 100_000 values (because the numerical simulation is not stabilized yet)
+    df.drop(columns=f'x{i}', inplace=True)
+    df = df.iloc[100_000:, :]
+    assert df.shape == (900_000, nb_variables)
+
+
     return df
 
 if __name__ == '__main__':
