e673cdda257c801566bd3075557a7acc67400855
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2026-05-19 15:07:06 +0200

    feature: improve plots for methodology

--- projects/gaussian_toy_model/slides_gaussian/plot_slide.py
+++ projects/gaussian_toy_model/slides_gaussian/plot_slide.py
@@ -1,10 +1,13 @@
+import random
 
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.random import normal
 from pip._internal.models import index
 from scipy.stats import norm
 
 from utils.utils_plot import show_or_save_plot
+from utils.utils_run import random_seed
 
 
 def loc_and_sigma(t, index: int):
@@ -20,7 +23,29 @@ def loc_and_sigma(t, index: int):
     else:
         raise NotImplementedError
 
-def plot_non_stationary_gaussian(index: int, show: bool = True):
+def plot_non_stationary_gaussian_samples(index: int, show: bool = True):
+    # Set random seed
+    random.seed(random_seed)
+    # Range of values
+    t_values = np.linspace(0, 3, 4)  # 5 pas de temps : 0, 2.5, 5, 7.5, 10
+
+    ax = plt.gca()
+    # Pour chaque t, tracer la densité gaussienne verticalement, centrée sur t
+    for t in t_values:
+        loc, sigma = loc_and_sigma(t, index)
+        values = normal(loc, sigma, size=1)
+        ax.plot([t], values, marker='o', label="$\mu(t) = {}$\n$\sigma(t) = {}$".format(loc, sigma))
+
+    ymin, ymax = ax.get_ylim()
+    ax.set_ylim((ymin, ymax * 1.2))
+    ax.legend(ncol=4, prop={'size': 10.3}, loc='upper center')
+    ax.set_xlabel("$t$")
+    ax.set_xticks(t_values)
+
+    show_or_save_plot("non_stationary_gaussian_samples_{}".format(index), show=show)
+
+
+def plot_non_stationary_gaussian_distribution(index: int, show: bool = True):
 
     # Range of values
     t_values = np.linspace(0, 3, 4)  # 5 pas de temps : 0, 2.5, 5, 7.5, 10
@@ -37,8 +62,8 @@ def plot_non_stationary_gaussian(index: int, show: bool = True):
     ax.set_xlabel("$t$")
     ax.set_xticks(t_values)
 
-    show_or_save_plot("non_stationary_gaussian_{}".format(index), show=show)
+    show_or_save_plot("non_stationary_gaussian_distribution_{}".format(index), show=show)
 
 if __name__ == '__main__':
     for index in list(range(4))[::-1]:
-        plot_non_stationary_gaussian(index, show=False)
+        plot_non_stationary_gaussian_samples(index, show=False)
