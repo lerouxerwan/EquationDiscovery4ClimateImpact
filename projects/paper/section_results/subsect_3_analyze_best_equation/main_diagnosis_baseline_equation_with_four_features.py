f56a36ff834116ea9e68ecae2d69772e6f3c22a9
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2026-06-29 22:44:05 +0200

    feature: add analysis of ordered equations from the linear regression for the ssp scenarios

--- projects/paper/section_results/subsect_3_analyze_best_equation/main_diagnosis_baseline_equation_with_four_features.py
+++ projects/paper/section_results/subsect_3_analyze_best_equation/main_diagnosis_baseline_equation_with_four_features.py
@@ -1,5 +1,6 @@
 from data.utils_dataset.npp_season_v1 import get_dataset
 from data.utils_dataset.validation_split import ValidationSplit
+from emulator.ordered_equation import OrderedEquation
 from plot.by_split.plot_scatter import plot_scatter_side_by_side
 from plot.by_split.plot_scatter_and_time_series import plot_scatter_and_time_series
 from plot.by_split.plot_time_series_side_by_side import plot_time_series_side_by_side
@@ -17,6 +18,12 @@ def main_diagnosis_baseline_equation_with_four_selection_features(show: bool = F
     # plot_time_series_side_by_side(emulator, dataset, show=show)
     plot_scatter_and_time_series(emulator, dataset, show=show)
     # plot_climato_side_by_side(top_emulator, dataset, show=show)
+
+    ordered_equation = OrderedEquation(emulator.selected_expr, dataset.X_train, dataset.X_variable_names)
+    print(ordered_equation.sorted_weight_and_term)
+
+
+
 
 
 if __name__ == '__main__':
