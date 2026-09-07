cac7bd4a0829c1acda336eeb193185cc10c6d82d
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2026-09-07 11:43:57 +0200

    feature: compute ratio of linear equations

--- projects/paper/section_results/subsect_1_compare_optimization/main_check_equations.py
+++ projects/paper/section_results/subsect_1_compare_optimization/main_check_equations.py
@@ -14,6 +14,8 @@ def main_check_equations(show: bool):
     datasets = [get_dataset(validation_size=validation_size, validation_split=validation_split)
         for validation_split, validation_size in product(validation_splits, validation_sizes)]
     opt = get_opt()
+    ratio_linear_equations_list = []
+    average_complexity_selected_equation_list = []
     for dataset in datasets:
         emulator = opt.get_top_emulator(dataset.X_train, dataset.y_train, dataset.validation_mask,
                                                  dataset.X_variable_names, dataset.X_units, dataset.y_units)
@@ -21,6 +23,7 @@ def main_check_equations(show: bool):
         count_linear = 0
         complexity_list_for_non_linear_equations = []
         complexity_list_for_linear_equations = []
+        complexity_list = []
         df = emulator.run_.df_cv_results
         for equation, variable_names, complexity in zip(df['expr'], df['variable_names'], df['complexity']):
             if (len(variable_names) == 1) and (variable_names[0] == ''):
@@ -38,13 +41,14 @@ def main_check_equations(show: bool):
                 else:
                     print(expr)
                     complexity_list_for_non_linear_equations.append(complexity)
+                complexity_list.append(complexity)
 
-        print('Ratio of linear equations', 100 * count_linear / len(df))
-        print('Average complexity for non linear equations:', np.mean(complexity_list_for_non_linear_equations))
-        print('Average complexity for linear equations:', np.mean(complexity_list_for_linear_equations))
+        ratio_linear_equations_list.append(100 * count_linear / len(df))
+        average_complexity_selected_equation_list.append(np.mean(complexity_list))
+    print('Ratio of linear equations', np.mean(ratio_linear_equations_list), ratio_linear_equations_list)
+    print('Average complexity for selected equation:', np.mean(average_complexity_selected_equation_list), average_complexity_selected_equation_list)
 
-        """une idée plus avancée serait de verifier qu'il y a bien au moins une equation 
-        non linear pour chaque pareto front quand la complexité augmente"""
+
 
 if __name__ == '__main__':
     main_check_equations(False)
